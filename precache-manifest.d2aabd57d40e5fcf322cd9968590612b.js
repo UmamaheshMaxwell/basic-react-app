fd8a37ad4dd1bbd427f202ef94ec373ab6d532f0
Umamaheswararao Meka <uma@olivetech.com>
2019-07-12 14:02:58 +0530

    Updates

---
+++ precache-manifest.d2aabd57d40e5fcf322cd9968590612b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538f16852d0cd9c0b66cdfdf7e5c1fde",
+    "url": "/basic-react-app/index.html"
+  },
+  {
+    "revision": "cbf86fd877089bc25d6a",
+    "url": "/basic-react-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "41bc73738254ed51523e",
+    "url": "/basic-react-app/static/css/main.e83ea91e.chunk.css"
+  },
+  {
+    "revision": "cbf86fd877089bc25d6a",
+    "url": "/basic-react-app/static/js/2.d865a23f.chunk.js"
+  },
+  {
+    "revision": "41bc73738254ed51523e",
+    "url": "/basic-react-app/static/js/main.02ae7580.chunk.js"
+  },
+  {
+    "revision": "24f6c4543be5b4791cb1",
+    "url": "/basic-react-app/static/js/runtime~main.5bd45cfd.js"
+  }
+]);
